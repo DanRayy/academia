b1db548732ee1b20f395ac5418746d06bd4867e1
Rays <148717985+DanRayy@users.noreply.github.com>
2025-12-01 00:15:08 -0300

    Refactor DataBaseConnection class formatting

--- src/main/java/br/com/academia/model/DataBaseConnection.java
+++ src/main/java/br/com/academia/model/DataBaseConnection.java
@@ -4,17 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DataBaseConnection {
+public class DataBaseConnection{
 	private static final String URL = "jdbc:mysql://localhost:3306/academiadb";
     private static final String USER = "admin";
     private static final String PASSWORD = "admin";
 
-    public static Connection getConnection() throws SQLException {
-        try {
+    public static Connection getConnection() throws SQLException{
+        try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
             
-        } catch (ClassNotFoundException e) {
+        }catch(ClassNotFoundException e){
             throw new SQLException("Driver JDBC não encontrado", e);
         }
     }
