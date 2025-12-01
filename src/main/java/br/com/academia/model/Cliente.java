aca9bea8539a391851a659f5a8328c4e6547d17b
Rays <148717985+DanRayy@users.noreply.github.com>
2025-12-01 00:14:35 -0300

    Format Cliente class and method signatures

--- src/main/java/br/com/academia/model/Cliente.java
+++ src/main/java/br/com/academia/model/Cliente.java
@@ -1,49 +1,49 @@
 package br.com.academia.model;
 
-public class Cliente {
+public class Cliente{
 	private int id;
 	private String nome;
 	private String telefone;
 	private String plano;
 	
-	public Cliente() {
+	public Cliente(){
 	}
 	
-	public Cliente (String nome, String telefone, String plano) {
+	public Cliente(String nome, String telefone, String plano){
 		this.nome = nome;
 		this.telefone = telefone;
 		this.plano = plano;
 	}
 	
-	public Cliente(int id, String nome, String telefone, String plano) {
+	public Cliente(int id, String nome, String telefone, String plano){
         this.id = id;
         this.nome = nome;
         this.telefone = telefone;
         this.plano = plano;
     }
 	
-	public int getId() {
+	public int getId(){
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(int id){
 		this.id = id;
 	}
-	public String getNome() {
+	public String getNome(){
 		return nome;
 	}
-	public void setNome(String nome) {
+	public void setNome(String nome){
 		this.nome = nome;
 	}
-	public String getTelefone() {
+	public String getTelefone(){
 		return telefone;
 	}
-	public void setTelefone(String telefone) {
+	public void setTelefone(String telefone){
 		this.telefone = telefone;
 	}
-	public String getPlano() {
+	public String getPlano(){
 		return plano;
 	}
-	public void setPlano(String plano) {
+	public void setPlano(String plano){
 		this.plano = plano;
 	}
 	
